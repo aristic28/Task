81663ad53f9bec0e72491daa1d78e8ac111530da
Andrijana Ristic <andrijanaristic28@gmail.com>
2022-02-25 11:55:36 +0100

    user class

--- Day17Task03/src/domain/User.java
+++ Day17Task03/src/domain/User.java
@@ -1,5 +1,48 @@
 package domain;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String role;
 
+    public User(String firstName, String lastName, String username, String password, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
