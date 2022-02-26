a84fcf93b9886ed17de41f36c9d15a023fc8f597
Andrijana Ristic <andrijanaristic28@gmail.com>
2022-02-26 20:51:29 +0100

    main method

--- Day17Task03/src/main/Start.java
+++ Day17Task03/src/main/Start.java
@@ -1,10 +1,25 @@
 package main;
 
+import domain.AllUsers;
+import domain.Role;
+import domain.User;
+import editingCollection.EditAdmin;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<User> list=new ArrayList<>(); //
 
+		AllUsers allUsers=new AllUsers(list);
+		EditAdmin editAdmin=new EditAdmin();
+		editAdmin.addUser();
+		editAdmin.addUser();
+		//editAdmin.addUser();
+		editAdmin.printAllUsers();
 	}
 
 }
